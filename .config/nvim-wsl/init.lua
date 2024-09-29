1692bff03b5bac5e038905dba1289998d0e3086b
Tom Simmons <tomasimmons@gmail.com>
2024-09-29 16:12:21 -0400

    Add conjure and supporting plugins

--- .config/nvim-wsl/init.lua
+++ .config/nvim-wsl/init.lua
@@ -13,8 +13,24 @@ vim.o.shiftwidth = 2 -- spaces per indent level
 -- loading lazy.nvim so that mappings are correct.
 require("lazy").setup({
   spec = {
-    -- import your plugins
-    { import = "plugins" },
+    {
+      {
+        "ggandor/leap.nvim",
+         dependencies = {
+          "tpope/vim-repeat",
+        },
+      },
+      "Mofiqul/vscode.nvim",
+      "Olical/conjure",
+      "williamboman/mason.nvim", -- installs language servers (:MasonInstall my-lsp)
+      "nvim-treesitter/nvim-treesitter", -- language parsing support, used by conjure but not a hard dep
+      {
+        "clojure-vim/vim-jack-in", -- provides :Clj command to start nREPL server
+        dependencies = {
+          "tpope/vim-dispatch",
+        },
+      }
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -24,6 +40,7 @@ require("lazy").setup({
 })
 
 require("vscode").load("dark")
+require("mason").setup()
 
 require("leap").create_default_mappings()
 vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
